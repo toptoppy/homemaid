04d354527e174c121db5ab02c2195c1f72c9e18d
toptoppy <toptoppy@odds.team>
2020-07-18 10:44:01 +0700

    Add function fizzbuzz case fizzbuzz

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -1,7 +1,9 @@
 import unittest
 
 def fizzbuzz(number):
-    if number % 3 == 0:
+    if number % 3 == 0 and number % 5 == 0:
+        return 'FizzBuzz'
+    elif number % 3 == 0:
         return 'Fizz'
 
 class TestFizzBuzz(unittest.TestCase):
@@ -12,5 +14,14 @@ class TestFizzBuzz(unittest.TestCase):
     def test_input_6_should_get_fizz(self):
         result = fizzbuzz(6)
         self.assertEqual(result,'Fizz')
+
+    def test_input_15_should_get_fizzbizz(self):
+        result = fizzbuzz(15)
+        self.assertEqual(result, 'FizzBuzz')
+    
+    def test_input_450_should_get_fizzbizz(self):
+        result = fizzbuzz(450)
+        self.assertEqual(result, 'FizzBuzz')
+
 
 unittest.main()
