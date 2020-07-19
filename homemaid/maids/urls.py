488986680669815467f04c69c3429aa539989e3d
toptoppy <toptoppy@odds.team>
2020-07-19 10:41:56 +0700

    Create template View ♠️

--- homemaid/maids/urls.py
+++ homemaid/maids/urls.py
@@ -4,5 +4,5 @@ from .views import MaidListView, maid_another_list_view
 
 urlpatterns = [
     path('', MaidListView.as_view(), name='maid-list'),
-    path('', maid_another_list_view, name='maid-another-list')
+    path('v2/', maid_another_list_view, name='maid-another-list')
 ]
--- homemaid/maids/views.py
+++ homemaid/maids/views.py
@@ -1,10 +1,20 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
 
 from .models import Maid
 
 class MaidListView(View):
+    template_name = 'maid_list.html'
+
     def get(self, request):
+        context ={
+            'maid_list' : Maid.objects.all()
+        }
+        return render(request, self.template_name, context)
+
+def maid_another_list_view(request):
+    if request.method == 'GET':
         maids = Maid.objects.all()
         html = ''
 
@@ -12,7 +22,3 @@ class MaidListView(View):
             html += f'<li>{maid.name}</li>'
         
         return HttpResponse(html)
-    
-
-def maid_another_list_view(request):
-    return HttpResponse()
